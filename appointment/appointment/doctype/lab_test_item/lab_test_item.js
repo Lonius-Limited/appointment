17875a846b141a565fb91b88b4ba47198682ff56
Salim Mwaura <dsmwaura@gmail.com>
2021-05-02 16:32:10 +0000

    Stable build of the appointment app

---
+++ appointment/appointment/doctype/lab_test_item/lab_test_item.js
@@ -0,0 +1,6 @@
+frappe.ui.form.on('Lab Test Item', {
+	refresh(frm) {
+		// your code here
+        console.log("I am hereat the lab test");
+	}
+})
---
+++ appointment/appointment/doctype/sub_county/test_sub_county.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Lonius Health & MTRH and contributors and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestSubCounty(unittest.TestCase):
+	pass
